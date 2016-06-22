56800a9254da207a799f8378c262121463cf460d
Andrew McWatters <mcwattersandrew@gmail.com>
2016-06-22 08:08:44 -0700

    Updated articles.lua

--- scripts/docs/articles.lua
+++ scripts/docs/articles.lua
@@ -31,29 +31,6 @@ local function getFirstLuaFunction( t )
 	end
 end
 
-local function r_purpose( section )
-	-- HACKHACK: Unify network purpose description.
-	if ( section == "network" ) then
-		return "Network interface"
-	end
-
-	local v = docs.findModule( section )
-	if ( v.__type ) then
-		if ( not v[ section ] ) then
-			v = getFirstLuaFunction( v )
-		else
-			v = v[ section ]
-		end
-	else
-		v = getFirstLuaFunction( v )
-	end
-
-	local src = debug.getinfo( v, "S" ).short_src
-	src = filesystem.read( src )
-	local _, _, purpose = find( src, "-- Purpose:%s(.-\r\n)" )
-	return purpose
-end
-
 local function r_header2( header )
 	local md = {}
 	insert( md, header )
@@ -67,76 +44,75 @@ local function isPanel( module )
 	return typeof( v, "panel" ) or v == gui.panel
 end
 
-local function r_constructor( section )
+local function r_constructor( article )
 	local md = {}
 	insert( md, r_header2( "Constructor" ) )
-	local modname = isPanel( section ) and "gui" or section
+	local modname = isPanel( article ) and "gui" or article
 	insert(
 		md,
-		"* [`" .. ( modname == "gui" and "gui." or "" ) .. section .. "()`]" ..
-		"(" .. modname .. "." .. section .. ")"
+		"* [`" .. ( modname == "gui" and "gui." or "" ) .. article .. "()`]" ..
+		"(" .. modname .. "." .. article .. ")"
 	)
 	insert( md, "" )
 	return concat( md, "\r\n" )
 end
 
-local function r_methods( section, methods, separator )
+local function r_methods( article, methods, separator )
 	local md = {}
 	for _, method in ipairs( methods ) do
 		insert(
 			md,
-			"* [`" .. section .. separator .. method .. "()`]" ..
-			"(" .. section .. "." .. method .. ")"
+			"* [`" .. article .. separator .. method .. "()`]" ..
+			"(" .. article .. "." .. method .. ")"
 		)
 	end
 	insert( md, "" )
 	return concat( md, "\r\n" )
 end
 
-local function writeArticle( section )
+local function writeArticle( article )
 	local md = {}
-	insert( md, r_header( section ) )
-	insert( md, r_purpose( section ) )
+	insert( md, r_header( article ) )
 
-	local v = docs.findModule( section )
+	local v = docs.findModule( article )
 	if ( v.__type ) then
 		if ( #docs.getClassMethods( v ) > 0 ) then
 			insert( md, r_header2( "Class Methods" ) )
-			insert( md, r_methods( section, docs.getClassMethods( v ), "." ) )
+			insert( md, r_methods( article, docs.getClassMethods( v ), "." ) )
 		end
 
-		insert( md, r_constructor( section ) )
+		insert( md, r_constructor( article ) )
 
 		if ( #docs.getMethods( v ) > 0 ) then
 			insert( md, r_header2( "Methods" ) )
-			insert( md, r_methods( section, docs.getMethods( v ), ":" ) )
+			insert( md, r_methods( article, docs.getMethods( v ), ":" ) )
 		end
 
 		if ( #docs.getCallbacks( v ) > 0 ) then
 			insert( md, r_header2( "Callbacks" ) )
-			insert( md, r_methods( section, docs.getCallbacks( v ), ":" ) )
+			insert( md, r_methods( article, docs.getCallbacks( v ), ":" ) )
 		end
 	else
 		if ( #docs.getAllMethods( v ) > 0 ) then
 			insert( md, r_header2( "Methods" ) )
-			insert( md, r_methods( section, docs.getAllMethods( v ), "." ) )
+			insert( md, r_methods( article, docs.getAllMethods( v ), "." ) )
 		end
 
 		if ( #docs.getCallbacks( v ) > 0 ) then
 			insert( md, r_header2( "Callbacks" ) )
-			insert( md, r_methods( section, docs.getCallbacks( v ), "." ) )
+			insert( md, r_methods( article, docs.getCallbacks( v ), "." ) )
 		end
 	end
 
 	md = concat( md, "\r\n" )
-	filesystem.write( "docs/" .. section .. ".md", md )
+	filesystem.write( "docs/" .. article .. ".md", md )
 end
 
-local sections = {}
-table.append( sections, docs.getClasses() )
-table.append( sections, docs.getInterfacesAndLibraries() )
-table.append( sections, docs.getPanels() )
+local articles = {}
+table.append( articles, docs.getClasses() )
+table.append( articles, docs.getInterfacesAndLibraries() )
+table.append( articles, docs.getPanels() )
 
-for _, section in ipairs( sections ) do
-	writeSection( section )
+for _, article in ipairs( articles ) do
+	writeArticle( article )
 end
